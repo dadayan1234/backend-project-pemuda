e5d687b66dd6f5a2e68526d2748a624378058d83
dadayan1234 <slasher001@outlook.com>
2025-06-24 05:31:19 +0700

    fix: update database engine configuration for improved connection handling

--- core/database.py
+++ core/database.py
@@ -13,7 +13,13 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=20,         # Jumlah koneksi standar di pool (default: 5)
+    max_overflow=50,      # Jumlah koneksi "sementara" tambahan (default: 10)
+    pool_timeout=30       # Waktu (detik) menunggu koneksi sebelum error (default: 30)
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
