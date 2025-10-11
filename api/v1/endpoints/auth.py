7386d4b440cbb5f6ee453a32756a763964db8ac2
dadayan1234 <slasher001@outlook.com>
2025-10-11 19:07:57 +0700

    try: with no admin req at admin register

--- api/v1/endpoints/auth.py
+++ api/v1/endpoints/auth.py
@@ -17,7 +17,7 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 @router.post("/admin/register", summary="Register user with custom role", tags=["Admin Only"])
-@admin_required()
+# @admin_required()
 async def admin_register(user: UserCreateWithRole, db: Session = Depends(get_db)):
     if db.query(User).filter(User.username == user.username).first():
         raise HTTPException(status_code=400, detail="Username already registered")
