0a05107d45f9c714292cb4a6ce1a6e5117b7c6c3
dadayan1234 <slasher001@outlook.com>
2025-10-11 21:47:10 +0700

    add : domain CORS allowed

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://admin.pemudanambangan.site"],  # Adjust in production
+    allow_origins=["http://localhost:3000", "https://admin.pemudanambangan.site", "https://pemudanambangan.site"],  # Adjust in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
